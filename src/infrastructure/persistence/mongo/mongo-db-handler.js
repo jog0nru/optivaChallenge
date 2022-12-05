17ca98f9373ca96b1deddd7047a186ea6c879c2b
Jose <jgonzalez@abervian.es>
2022-12-05 19:22:05 +0100

    frontend an create indexes in mongoDB

--- src/infrastructure/persistence/mongo/mongo-db-handler.js
+++ src/infrastructure/persistence/mongo/mongo-db-handler.js
@@ -11,6 +11,18 @@ class MongoDbHandler {
           {useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: timeout});
       const db = await this.client.db(dbName);
 
+      db.collection('cards').createIndexes([
+        {name: 'name', key: {name: 1}},
+        {name: 'legality', key: {legalities: 1}},
+        {name: 'set', key: {set: 1}},
+      ], (err, result) => {
+        if (err) {
+          console.log(`There was an error while creating voices_id index due to: ${err}`);
+        } else {
+          console.log(`Index created: ${result}`);
+        }
+      });
+
       return db;
     } catch (err) {
       const error = err.message ? err.message : err;
